a664a2a84cc0211eca4df91a53d39041a91a9b80
Jen <73888356+berryjen@users.noreply.github.com>
2022-04-12 13:13:11 +0800

    created different paths with specific responses

--- app.js
+++ app.js
@@ -2,10 +2,44 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/array', (req, res) => {
+  res.json ([{
+    name: "country 1",
+    visited: true,
+  },
+  {
+    name: "country 2",
+    visited: false,
+  },
+  {
+    name: "country 3",
+    visited: true,
+  }
+  ]);
+});
 
-app.listen(port, () => {
+/* 
+/  : responds with list of all countries in the world
+/country: list of cities within the country object
+/country/city: responds with city object with "visited" property included
+
+*/
+app.get('/uk', (req, res) => {
+   res.json({
+     name: "England",
+     visited: true
+   });
+});
+
+app.get("/hello", (req, res) => {
+  res.send ("hello")
+});
+
+app.get("/hello/:name", (req, res) => {
+  res.send ("hello " + req.params.name)
+});
+
+app.listen(port, () => 
+{
   console.log(`Example app listening on port ${port}`)
 })
