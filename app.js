3f8abc9f99aaac765e9702ce3e314be96c4df366
Jen <73888356+berryjen@users.noreply.github.com>
2022-04-14 19:13:01 +0800

    created 3 pathways to return list of countries (visited & not), cities; array vs objs exercise

--- app.js
+++ app.js
@@ -22,8 +22,184 @@ app.get('/array', (req, res) => {
 /  : responds with list of all countries in the world
 /country: list of cities within the country object
 /country/city: responds with city object with "visited" property included
-
+create mental data structure on separate file
 */
+app.get("/", (req, res) => {
+  res.json ([{
+      country: "Spain",
+      },
+      {
+      country: "Nicaragua",
+      },
+      {
+      country: "Chile",
+      },
+      {
+      country: "Croatia",
+      },
+      {
+      country: "Colombia",
+      },
+      {
+      country: "Argentina",
+      },
+      {
+      country: "Morocco",
+      },
+      {
+      country: "Japan",
+      },
+      {
+      country: "Turkey",
+      },
+      {
+      country: "Australia",
+    }
+  ]);
+});  
+
+app.get('/country', (req, res) => {
+   res.json([{
+     country: "Spain",
+   },
+   {
+     city: "San Sebastian",
+   },
+   {
+     city: "Barcelona",
+   },
+   {
+     city: "Cordoba",
+   },
+   {
+     city: "Granada",
+   },
+   {
+     city: "Nerja",
+   },
+   {
+     city: "Seville",
+   },
+   {
+     city: "Malaga",
+   },
+   {
+     city: "Marbella",
+   },
+   {
+     city: "Mallorca"
+   },
+   {
+     country: "Nicaragua",
+   },
+   {
+   city: "Leon",
+   },
+   {
+    city: "San Juan del Sur",
+   },
+   {
+     country: "Chile",
+   },
+   {
+     city: "none",
+   },
+   {
+     country: "Croatia",
+   },
+   {
+     city: "Rijeka",
+   },
+   {
+     city: "Novalija",
+   },
+   {
+     city: "Zadar",
+   },
+   {
+     city: "Sibenik",
+   },
+   {
+     city: "Split",
+   },
+   {
+     city: "Makarska",
+   },
+   {
+     city: "Dubrovnik",
+   },
+   {
+     country: "Colombia",
+     city: "none",
+   },
+   {
+     country: "Argentia",
+     city: "none",
+   },
+   {
+     country: "Morocco",
+   },
+   {
+     city: "Fes",
+   },
+   {
+     city: "Rabat",
+   },
+   {
+     city: "Merzouga",
+   },
+   {
+     city: "Marrakech",
+   },
+   {
+     country: "Japan",
+   },
+   {
+     city: "Tokyo",
+   },
+   {
+     city: "Osaka",
+   },
+   {
+     city: "Kyoto",
+   },
+   {
+     city: "Kumayama",
+   },
+   {
+     country: "Turkey",
+   },
+   {
+     city: "none",
+   },
+   {
+     country: "Australia",
+   },
+   {
+     city: "none",
+   }
+  ]);
+});
+
+app.get('/country/city', (req, res) => {
+   res.json({
+     name: "Madrid",
+     visited: false,
+     name: "Barcelona",
+     visited: true,
+     name: "Chefchaouen",
+     visited: false,
+     name: "Makarska",
+     visited: true,
+     name: " Santiego",
+     visited: false,
+     name: "Buenos Aires",
+     visited: false,
+     name: "Okinawa",
+     visited: false,
+   });
+});
+
 app.get('/uk', (req, res) => {
    res.json({
      name: "England",
@@ -43,3 +219,4 @@ app.listen(port, () =>
 {
   console.log(`Example app listening on port ${port}`)
 })
+
---
+++ array vs objs.js
@@ -0,0 +1,18 @@
+array = [
+ "purple",
+ "orange",
+ "blue",
+]
+object = {
+	"colors": ["white", "brown", "blue"],
+	"price": 6,
+	"ingredients": [{
+			"name": "dark chocolate",
+			"quantity": 70
+		},
+		{
+			"name": "milk",
+			"quantity": 30
+		}
+	]
+}
