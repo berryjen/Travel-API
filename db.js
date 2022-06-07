cea4c06d73829eb321622cfd7113c2db23ad9fd5
Jen <73888356+berryjen@users.noreply.github.com>
2022-06-07 14:16:57 +0800

    fixed country db.each function

--- db.js
+++ db.js
@@ -5,9 +5,9 @@
 // 6/1/2022
 //write query to return all countries
   // --> change SELECT query; what from what (country from countries table)
-  // console.log(row.country)
+  // console.log(country)
   // return a list of cities from Tunisia from new db.each that will display on con
-  // return all cities
+  // return all cities with the =>  notation
   // return all cities from any country
   // return all cities from the given country (possible documentation look-up)
 const sqlite3 = require('sqlite3').verbose()
@@ -15,10 +15,19 @@ const db = new sqlite3.Database('lorem.db')
 const requested_country= 'Tunisia'
 
 db.serialize(() => {
-  db.each('SELECT rowid AS id, info FROM lorem', (err, row) => {
+  /*db.each('SELECT rowid AS id, info FROM lorem', (err, row) => {
     console.log(`${row.id}: ${row.info}`)
-  })
-  //db.each
+  }) */
+  db.each('SELECT country FROM countries',
+    function(err, row) {
+      console.log(`${row.country}`)
+    } 
+  )
+  /*db.each('SELECT city FROM cities',
+    console.log('${cities.city}')
+  )
+  db.each('SELECT city FROM cities, UNION SELECT country FROM countries',
+    console.log('${cities.city}', '${countries.country')
+  )*/
 })
-
 db.close()
