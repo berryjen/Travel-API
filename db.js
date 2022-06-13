fac764d31c5b28e783cf0ed8a48d98db079c8659
Jen <73888356+berryjen@users.noreply.github.com>
2022-06-13 23:49:23 +0800

    updated LIKE query & specific city query

--- db.js
+++ db.js
@@ -12,10 +12,11 @@
   // return all cities from the given country (possible documentation look-up)
   
   // filter countries LIKE  (to get a partial match)
-  // each API call has a different query in app.js......what sqlite query do I want to write to return a specfic country with a city found within it
+  // each API call has a different query in app.js......what sql query do I want to write to return a specfic country with a city found within it
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('lorem.db')
 const requested_country= 'Tunisia'
+const requested_city = 'Tunis'
 
 db.serialize(() => {
   /*db.each('SELECT rowid AS id, info FROM lorem', (err, row) => {
@@ -31,7 +32,7 @@ db.serialize(() => {
       console.log(`${row.city}`)
     }
   ) */
-   db.each('SELECT city, country FROM cities JOIN countries ON cities.country_id = countries.id WHERE country= ?', requested_country, 
+    /* db.each('SELECT city, country FROM cities JOIN countries ON cities.country_id = countries.id WHERE country= ?', requested_country, 
     (err, row) => {
          if (err) {
           console.log(err)
@@ -40,7 +41,27 @@ db.serialize(() => {
           console.log(`${row.country}: ${row.city}`)
          }
     }
-   )
+   ) */
+  /* db.each("SELECT city, country FROM cities JOIN countries ON cities.country_id = countries.id WHERE country LIKE'%land'",
+    (err, row) => {
+         if (err) {
+          console.log(err)
+         }
+         else {
+          console.log(`${row.country}: ${row.city}`)
+         }
+    }
+  ) */
+   db.each('SELECT * FROM cities JOIN countries ON cities.country_id = countries.id WHERE country = ? AND city = ?', requested_country, requested_city,
+    (err, row) => {
+         if (err) {
+          console.log(err)
+         }
+         else {
+          console.log(`${row.country}: ${row.city}`)
+         }
+    }
+   ) 
 })
 db.close()
 
